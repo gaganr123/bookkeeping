25410c20de6ea01f229625151fec1772537bf72f
suvas magar <suvas.magar.69@gmail.com>
2022-11-01 15:10:43 +0545

    Check commit

--- demo.js
+++ demo.js
@@ -5,7 +5,7 @@ let services = document.getElementById("services");
 let about = document.getElementById("about");
 let pricing = document.getElementById("pricing");
 let partnership = document.getElementById("partnership");
-let faqs = document.getElementById("faqs");
+let contact = document.getElementById("contact");
 let blog = document.getElementById("blog");
 
 window.onscroll = function () {
@@ -16,17 +16,9 @@ window.onscroll = function () {
 function changeColor() {
     document.getElementById("navbar").style.backgroundColor = "#E4812F";
     document.getElementById("navImg").src = "imgs/changeimg.png";
-    // document.getElementById("navImg").src = "imgchange.png";
 
 
     if (window.scrollY == 0) {
-        // $(function() {      
-        //     let isMobile = window.matchMedia("only screen and (max-width: 320px)").matches;
-        
-        //     if (isMobile) {
-        //         changeColorto();
-        //     }
-        //  });
         changeColorto();
     }
     else{
@@ -39,8 +31,8 @@ function changeColor() {
         pricing.addEventListener('mouseleave', check2 = () => { pricing.style.color = "#000000"});
         partnership.addEventListener('mouseenter', check1 = () => { partnership.style.color = "#FFFFFF"});
         partnership.addEventListener('mouseleave', check2 = () => { partnership.style.color = "#000000"});
-        faqs.addEventListener('mouseenter', check1 = () => { faqs.style.color = "#FFFFFF"});
-        faqs.addEventListener('mouseleave', check2 = () => { faqs.style.color = "#000000"});
+        contact.addEventListener('mouseenter', check1 = () => { contact.style.color = "#FFFFFF"});
+        contact.addEventListener('mouseleave', check2 = () => { contact.style.color = "#000000"});
         blog.addEventListener('mouseenter', check1 = () => { blog.style.color = "#FFFFFF"});
         blog.addEventListener('mouseleave', check2 = () => { blog.style.color = "#000000"});
        }
@@ -49,7 +41,6 @@ function changeColor() {
 function changeColorto() {
     Xpert.style.color = "#EE6600"
     document.getElementById("navImg").src = "imgs/logo.png";
-
     document.getElementById("navbar").style.cssText = "backgroundColor: none;"
     services.addEventListener('mouseenter', check1 = () => { services.style.color = "#F15A22"});
     services.addEventListener('mouseleave', check2 = () => { services.style.color = "#000000"});
@@ -59,8 +50,8 @@ function changeColorto() {
     pricing.addEventListener('mouseleave', check2 = () => { pricing.style.color = "#000000"});
     partnership.addEventListener('mouseenter', check1 = () => { partnership.style.color = "#F15A22"});
     partnership.addEventListener('mouseleave', check2 = () => { partnership.style.color = "#000000"});
-    faqs.addEventListener('mouseenter', check1 = () => { faqs.style.color = "#F15A22"});
-    faqs.addEventListener('mouseleave', check2 = () => { faqs.style.color = "#000000"});
+    contact.addEventListener('mouseenter', check1 = () => { contact.style.color = "#F15A22"});
+    contact.addEventListener('mouseleave', check2 = () => { contact.style.color = "#000000"});
     blog.addEventListener('mouseenter', check1 = () => { blog.style.color = "#F15A22"});
     blog.addEventListener('mouseleave', check2 = () => { blog.style.color = "#000000"});
 }
